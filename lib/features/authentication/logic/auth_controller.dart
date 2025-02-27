bec343e7d263f30add3db9488fea8dcfd119be2e
shaon <shaonx@proton.me>
2025-02-27 13:03:56 +0600

    add sign in method

--- lib/features/authentication/logic/auth_controller.dart
+++ lib/features/authentication/logic/auth_controller.dart
@@ -4,29 +4,58 @@ import 'package:get/get.dart';
 class AuthController extends GetxController {
   Rx<RxStatus> status = Rx<RxStatus>(RxStatus.success());
 
-
-  Future<void> createUserWithEmailAndPassword(String emailAddress, String password) async {
+  Future<void> createUserWithEmailAndPassword(
+      String emailAddress, String password) async {
     status.value = RxStatus.loading();
 
     try {
-      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: emailAddress,
         password: password,
       );
+
       status.value = RxStatus.success();
-      
+
       // show toast
-      Get.snackbar('', 'Your account has been successfully created. You can now log in.');
+      Get.snackbar('',
+          'Your account has been successfully created. You can now log in.');
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
         status.value = RxStatus.error('The password provided is too weak.');
       } else if (e.code == 'email-already-in-use') {
-        status.value = RxStatus.error('The account already exists for that email.');
+        status.value =
+            RxStatus.error('The account already exists for that email.');
       } else {
         status.value = RxStatus.error(e.toString());
       }
     } catch (e) {
       status.value = RxStatus.error(e.toString());
     }
+  }
+
+  Future<bool> signInWithEmailAndPassword(
+      String emailAddress, String password) async {
+    status.value = RxStatus.loading();
+
+    try {
+      final credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: emailAddress, password: password);
+
+      status.value = RxStatus.success();
+      return true;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        status.value = RxStatus.error('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        status.value = RxStatus.error('Wrong password provided for that user.');
+      } else {
+        status.value = RxStatus.error(e.toString());
+      }
+    } catch (e) {
+      status.value = RxStatus.error(e.toString());
+    }
+
+    return false;
   }
 }
--- lib/features/authentication/pages/login.dart
+++ lib/features/authentication/pages/login.dart
@@ -14,73 +14,87 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Login",
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+      body: Obx(() {
+        if (authController.status.value.isLoading) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (authController.status.value.isError) {
+          return Center(
+            child: Text(authController.status.value.errorMessage ?? "Something went wrong.. please try again later."),
+          );
+        } else {
+          return Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Login",
+                      style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+
+
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: emailValidator,
+                    ),
+                    SizedBox(height: 10),
+
+
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        border: OutlineInputBorder(),
+                      ),
+                      obscureText: true,
+                      validator: passwordValidator,
+                    ),
+                    SizedBox(height: 20),
+
+
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          if (formKey.currentState!.validate()) {
+                            if (await authController.signInWithEmailAndPassword(emailController.text, passwordController.text)) {
+                              Navigator.pushReplacementNamed(context, '/home');
+                            }
+                          }
+                        },
+                        child: Text("Login"),
+                      ),
+                    ),
+
+                    SizedBox(height: 20),
+
+
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/signup');
+                      },
+                      child: Text(
+                        "Don't have an account? Sign Up",
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 20),
-
-                
-                TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: emailValidator,
-                ),
-                SizedBox(height: 10),
-
-                
-                TextFormField(
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    border: OutlineInputBorder(),
-                  ),
-                  obscureText: true,
-                  validator: passwordValidator,
-                ),
-                SizedBox(height: 20),
-
-                
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (formKey.currentState!.validate()) {
-
-                      }
-                    },
-                    child: Text("Login"),
-                  ),
-                ),
-
-                SizedBox(height: 20),
-
-                
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, '/signup');
-                  },
-                  child: Text(
-                    "Don't have an account? Sign Up",
-                    style: TextStyle(color: Colors.blue),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
+          );
+        }
+      },),
     );
   }
 }
